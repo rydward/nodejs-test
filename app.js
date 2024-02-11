7d5d9a18758d71ba9a974799894effd9b2881fa6
Lucas Lemal <lucas.lemal2002@gmail.com>
2024-02-11 22:36:26 +0100

    add file

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 // Importer le module http
 const http = require('http');
+const server = require('./index');
 
-// Créer un objet serveur
-const server = http.createServer((req, res) => {
-    // Définir le code de statut et le type de contenu de la réponse
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    // Envoyer un message HTML dans la réponse
-    res.end('<h1>Bonjour, monde!</h1>');
+describe('Server', () => {
+  afterAll((done) => {
+    server.close(done);
+    });
+
+    it('responds', (done) => {
+      http.get('http://localhost:3000', (response) => {
+        expect(response.statusCode).toBe(200);
+        let data = '';
+        response.on('data', (chunk) => {
+          data += chunk;
+        });
+        response.on('end', () => {
+          expect(data).toBe('Hello World!');
+          done();
+        }); 
+      });
+    });
 });
 
 // Écouter le port 3000
